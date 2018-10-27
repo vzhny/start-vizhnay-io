68a702d86bbd8915009b022e1969f05caa5a2962
Diego Vizhnay <diego@vzhny.com>
2018-10-27 01:44:26 -0400

    Added an opening animation to the Modal component | 10/27

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Form from '../../containers/Form/Form';
 import Card from '../Card/Card';
 import styles from './Modal.module.scss';
 
 const Modal = props => (
-  <div>
+  <>
     <div className={styles.Backdrop} onClick={props.clicked} />
     <div className={styles.Modal}>
       <Card title="Add Link">
         <Form clicked={props.clicked} linksUpdated={props.linksUpdated} />
       </Card>
     </div>
-  </div>
+  </>
 );
-
-export default Modal;
 
 Modal.propTypes = {
   clicked: PropTypes.func.isRequired,
   linksUpdated: PropTypes.func.isRequired,
 };
+
+export default Modal;
