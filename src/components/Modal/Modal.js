505bcc496075e5210ce1a1f8e0a5a814d1661524
Diego Vizhnay <diego@vzhny.com>
2019-02-01 16:08:03 -0500

    Added escape-key-to-close functionality to the modal using hooks | 02/01

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Form from '../../containers/Form/Form';
 import Card from '../Card/Card';
 import styles from './Modal.module.scss';
 
-const Modal = ({ clicked, linksUpdated }) => (
-  <>
-    <div className={styles.Backdrop} onClick={clicked} />
-    <div className={styles.Modal}>
-      <Card title="Add Link">
-        <Form clicked={clicked} linksUpdated={linksUpdated} />
-      </Card>
-    </div>
-  </>
-);
+const Modal = ({ clicked, linksUpdated }) => {
+  const handleEscKey = e => {
+    const keys = {
+      27: () => {
+        e.preventDefault();
+        clicked();
+        window.removeEventListener('keyup', handleEscKey);
+      },
+    };
+
+    if (keys[e.keyCode]) {
+      keys[e.keyCode]();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keyup', handleEscKey);
+
+    return () => {
+      window.removeEventListener('keyup', handleEscKey);
+    };
+  });
+
+  return (
+    <>
+      <div className={styles.Backdrop} onClick={clicked} />
+      <div className={styles.Modal}>
+        <Card title="Add Link">
+          <Form clicked={clicked} linksUpdated={linksUpdated} />
+        </Card>
+      </div>
+    </>
+  );
+};
 
 Modal.propTypes = {
   clicked: PropTypes.func.isRequired,
