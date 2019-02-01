ad28112b655c09d6bd1541639fdac3aa90a95476
Diego Vizhnay <diego@vzhny.com>
2019-02-01 17:56:06 -0500

    Refactored the form component to use hooks | 02/01

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import store from 'store';
 import styles from './Form.module.scss';
@@ -7,32 +7,30 @@ import find from 'lodash/find';
 import findIndex from 'lodash/findIndex';
 import { unslugify } from '../../utils/utils';
 
-export default class Form extends Component {
-  state = {
-    linkData: {
-      name: '',
-      url: '',
-      category: '',
+const Form = ({ clicked, linksUpdated }) => {
+  const [linkData, setLinkData] = useState({
+    name: '',
+    url: '',
+    category: '',
+  });
+  const [categories, setCategories] = useState([]);
+  const [showAddNewCategory, toggleShowAddNewCategory] = useState(false);
+  const [validationErrors, setValidationErrors] = useState({
+    name: {
+      alreadyExists: false,
+      missing: false,
     },
-    categories: [],
-    showAddNewCategory: false,
-    validationErrors: {
-      name: {
-        alreadyExists: false,
-        missing: false,
-      },
-      url: {
-        alreadyExists: false,
-        missing: false,
-      },
-      category: {
-        alreadyExists: false,
-        missing: false,
-      },
+    url: {
+      alreadyExists: false,
+      missing: false,
     },
-  };
+    category: {
+      alreadyExists: false,
+      missing: false,
+    },
+  });
 
-  componentDidMount() {
+  useEffect(() => {
     let categoriesCollection = store.get('categoriesCollection');
 
     if (categoriesCollection === undefined) {
@@ -48,97 +46,77 @@ export default class Form extends Component {
       ];
     }
 
-    this.setState({
-      ...this.state,
-      categories: categoriesCollection,
-    });
-  }
+    setCategories([...categoriesCollection]);
+  });
 
-  inputHandler = event => {
+  const inputHandler = event => {
     const { name, value } = event.target;
 
     switch (name) {
       case 'name':
-        // Link Name input field
-        this.setState({
-          ...this.state,
-          linkData: {
-            ...this.state.linkData,
-            name: value,
-          },
+        setLinkData({
+          ...linkData,
+          name: value,
         });
         break;
       case 'url':
-        // Link URL input field
-        this.setState({
-          ...this.state,
-          linkData: {
-            ...this.state.linkData,
-            url: value,
-          },
+        setLinkData({
+          ...linkData,
+          url: value,
+        });
+        break;
+      case 'category':
+        setLinkData({
+          ...linkData,
+          category: slugify(value, { lower: true }),
         });
         break;
       case 'categories':
         if (value !== 'new-category') {
           // User selected an existing category
-          this.setState({
-            ...this.state,
-            linkData: {
-              ...this.state.linkData,
-              category: slugify(value, { lower: true }),
-            },
-            showAddNewCategory: false,
+          setLinkData({
+            ...linkData,
+            category: slugify(value, { lower: true }),
           });
+
+          toggleShowAddNewCategory(false);
         } else {
           // User selected to add a new category, show the input field
-          this.setState({
-            ...this.state,
-            showAddNewCategory: true,
-          });
+          toggleShowAddNewCategory(true);
         }
-        break;
-      case 'category':
-        // Link Category input field
-        this.setState({
-          ...this.state,
-          linkData: {
-            ...this.state.linkData,
-            category: slugify(value, { lower: true }),
-          },
-        });
         break;
       default:
         break;
     }
   };
 
-  onFormSubmitHandler = e => {
+  const onFormSubmitHandler = e => {
     e.preventDefault();
 
-    const formInvalid = this.checkForInvalidation();
+    const formInvalid = checkForInvalidation();
 
     if (formInvalid) {
       return;
     }
 
-    this.addLinkToCollection();
-    this.addCategoryToCollection();
+    addLinkToCollection();
+    addCategoryToCollection();
 
-    this.closeModal(true);
+    closeModal(true);
   };
 
-  closeModal = async close => {
+  const closeModal = async close => {
     if (close) {
-      await this.props.clicked();
-      await this.props.linksUpdated();
+      await clicked();
+      await linksUpdated();
     }
   };
 
-  checkForInvalidation = () => {
+  const checkForInvalidation = () => {
     // TODO check for any duplicate links attempting to be added
     let containsErrors = false;
-    const { name, url, category } = this.state.linkData;
-    const updatedValidationErrors = this.state.validationErrors;
+    const { name, url, category } = linkData;
+    const updatedValidationErrors = validationErrors;
 
     if (name.length === 0) {
       updatedValidationErrors.name.missing = true;
@@ -161,20 +139,14 @@ export default class Form extends Component {
       updatedValidationErrors.category.missing = false;
     }
 
-    this.setState({
-      ...this.state,
-      linkData: {
-        ...this.state.linkData,
-      },
-      validationErrors: updatedValidationErrors,
-    });
+    setValidationErrors(updatedValidationErrors);
 
     return containsErrors;
   };
 
-  addLinkToCollection = () => {
+  const addLinkToCollection = () => {
     let linksCollection = store.get('linksCollection');
-    const { name, url, category } = this.state.linkData;
+    const { name, url, category } = linkData;
 
     if (name.length === 0) {
     }
@@ -213,83 +185,82 @@ export default class Form extends Component {
     }
   };
 
-  addCategoryToCollection = () => {
+  const addCategoryToCollection = () => {
     let categoriesCollection = store.get('categoriesCollection');
 
     if (categoriesCollection === undefined) {
-      categoriesCollection = this.state.categories;
+      categoriesCollection = categories;
     }
 
-    const duplicateCategory = find(categoriesCollection, ['value', this.state.linkData.category]);
+    const duplicateCategory = find(categoriesCollection, ['value', linkData.category]);
 
     if (duplicateCategory === undefined) {
       const addedCategory = {
-        value: this.state.linkData.category,
-        title: unslugify(this.state.linkData.category),
+        value: linkData.category,
+        title: unslugify(linkData.category),
       };
 
       categoriesCollection.push(addedCategory);
 
       store.set('categoriesCollection', categoriesCollection);
 
-      this.setState({
-        ...this.state,
-        categories: categoriesCollection,
-      });
+      setCategories(categoriesCollection);
     }
   };
 
-  render() {
-    const dropDownOptions = this.state.categories.map((category, index) => {
-      return (
-        <option key={index} value={category.value}>
-          {category.title}
-        </option>
-      );
-    });
-
+  const dropDownOptions = categories.map((category, index) => {
     return (
-      <form className={styles.Form} onSubmit={e => this.onFormSubmitHandler(e)}>
-        <p>Please enter your link information below:</p>
-        <label className={styles.Label} htmlFor="name">
-          Link Name:
-        </label>
-        <input className={styles.Input} onChange={e => this.inputHandler(e)} type="text" name="name" />
-        {this.state.validationErrors.name.missing ? (
-          <p className={styles.FormError}>Please enter a link name.</p>
-        ) : null}
-        <label className={styles.Label} htmlFor="url">
-          Link URL:
-        </label>
-        <input className={styles.Input} onChange={e => this.inputHandler(e)} type="url" name="url" />
-        {this.state.validationErrors.url.missing ? <p className={styles.FormError}>Please enter a url.</p> : null}
-        <label className={styles.Label} htmlFor="category">
-          Link Category:
-        </label>
-        <select className={styles.Dropdown} name="categories" onChange={e => this.inputHandler(e)}>
-          {dropDownOptions}
-        </select>
-        {this.state.showAddNewCategory ? (
-          <input
-            className={styles.Input}
-            style={{ marginTop: '1rem' }}
-            onChange={e => this.inputHandler(e)}
-            type="text"
-            name="category"
-          />
-        ) : null}
-        {this.state.validationErrors.category.missing ? (
-          <p className={styles.FormError}>Please select or enter a category.</p>
-        ) : null}
-        <button className={styles.SubmitBtn} type="submit">
-          Add Link
-        </button>
-      </form>
+      <option key={index} value={category.value}>
+        {category.title}
+      </option>
     );
-  }
-}
+  });
+
+  return (
+    <form className={styles.Form} onSubmit={e => onFormSubmitHandler(e)}>
+      <p>Please enter your link information below:</p>
+      <label className={styles.Label} htmlFor="name">
+        Link Name:
+      </label>
+      <input className={styles.Input} onChange={e => inputHandler(e)} type="text" name="name" />
+      {validationErrors.name.missing ? (
+        <p className={styles.FormError}>Please enter a link name.</p>
+      ) : null}
+      <label className={styles.Label} htmlFor="url">
+        Link URL:
+      </label>
+      <input className={styles.Input} onChange={e => inputHandler(e)} type="url" name="url" />
+      {validationErrors.url.missing ? (
+        <p className={styles.FormError}>Please enter a url.</p>
+      ) : null}
+      <label className={styles.Label} htmlFor="category">
+        Link Category:
+      </label>
+      <select className={styles.Dropdown} name="categories" onChange={e => inputHandler(e)}>
+        {dropDownOptions}
+      </select>
+      {showAddNewCategory ? (
+        <input
+          className={styles.Input}
+          style={{ marginTop: '1rem' }}
+          onChange={e => inputHandler(e)}
+          type="text"
+          name="category"
+        />
+      ) : null}
+      {validationErrors.category.missing ? (
+        <p className={styles.FormError}>Please select or enter a category.</p>
+      ) : null}
+      <button className={styles.SubmitBtn} type="submit">
+        Add Link
+      </button>
+    </form>
+  );
+};
 
 Form.propTypes = {
   clicked: PropTypes.func.isRequired,
   linksUpdated: PropTypes.func.isRequired,
 };
+
+export default Form;
