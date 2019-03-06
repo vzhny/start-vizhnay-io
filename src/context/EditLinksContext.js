12cff14adb8ffca398e9892fe7c2c200e406d130
Diego Vizhnay <diego@vzhny.com>
2019-03-06 15:15:30 -0500

    Created an edit links context to toggle link remove button | 03/06

---
+++ src/context/EditLinksContext.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const EditLinksContext = React.createContext([false, () => {}]);
+
+export const EditLinksConsumer = EditLinksContext.Consumer;
+
+export const EditLinksProvider = ({ children }) => {
+  const [editable, toggleEditable] = useState(false);
+
+  return <EditLinksContext.Provider value={[editable, toggleEditable]}>{children}</EditLinksContext.Provider>;
+};
+
+EditLinksProvider.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+};
