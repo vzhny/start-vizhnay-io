24bdf556ff67a84085e653eae814df745311410e
Diego Vizhnay <diego@vzhny.com>
2019-03-06 15:11:18 -0500

    Hooked up edit link context to toggle remove button display | 03/06

--- src/components/UI/Link/Link.js
+++ src/components/UI/Link/Link.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { EditLinksContext } from '../../../context/EditLinksContext.js';
 import PropTypes from 'prop-types';
 import { linkContainer, link, removeBtn } from './Link.module.scss';
 
-const Link = ({ url, name, removeLink }) => (
-  <div className={linkContainer}>
-    <a className={link} target="_blank" rel="noopener noreferrer" href={url}>
-      {name}
-    </a>
-    <span className={removeBtn} onClick={removeLink}>
-      &times;
-    </span>
-  </div>
-);
+const Link = ({ url, name, removeLink }) => {
+  const [editable, toggleEditable] = useContext(EditLinksContext); // eslint-disable-line
+
+  return (
+    <div className={linkContainer}>
+      <a className={link} target="_blank" rel="noopener noreferrer" href={url}>
+        {name}
+      </a>
+      {editable && (
+        <span className={removeBtn} onClick={removeLink}>
+          &times;
+        </span>
+      )}
+    </div>
+  );
+};
 
 Link.propTypes = {
   name: PropTypes.string.isRequired,
