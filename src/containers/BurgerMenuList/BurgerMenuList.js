71fddf86ac06362a9c628e236bf6b84957345da1
Diego Vizhnay <diego@vzhny.com>
2019-03-07 11:52:04 -0500

    Added a new button to toggle the modal to add a new link, added credits to the bottom of the menu | 03/07

--- src/containers/BurgerMenuList/BurgerMenuList.js
+++ src/containers/BurgerMenuList/BurgerMenuList.js
@@ -1,13 +1,27 @@
 import React, { useContext } from 'react';
 import { EditLinksContext } from '../../context/EditLinksContext';
+import { ToggleModalContext } from '../../context/ToggleModalContext';
+import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import { burgerMenuList, burgerMenuListItem, menuButton } from './BurgerMenuList.module.scss';
+import { burgerMenuList, burgerMenuListItem, menuButton, linkButton, footerLink } from './BurgerMenuList.module.scss';
 
 const BurgerMenuList = ({ toggleSideMenu }) => {
   const [editable, toggleEditable] = useContext(EditLinksContext); // eslint-disable-line
+  const [showModal, toggleModal] = useContext(ToggleModalContext); // eslint-disable-line
 
   return (
     <ul className={burgerMenuList}>
+      <li className={burgerMenuListItem}>
+        <button
+          className={menuButton}
+          onClick={() => {
+            toggleModal(true);
+            toggleSideMenu(false);
+          }}
+        >
+          Add a new Link
+        </button>
+      </li>
       <li className={burgerMenuListItem}>
         <button
           className={menuButton}
@@ -18,6 +32,27 @@ const BurgerMenuList = ({ toggleSideMenu }) => {
         >
           Edit Links
         </button>
+      </li>
+      <li className={clsx(burgerMenuListItem, footerLink)}>
+        <span>
+          Created with{' '}
+          <span role="img" aria-label="black heart">
+            🖤
+          </span>{' '}
+          by{' '}
+        </span>
+        <a
+          className={linkButton}
+          href="https://vizhnay.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={() => {
+            toggleSideMenu(false);
+          }}
+        >
+          vzhny
+        </a>
+        <span>.</span>
       </li>
     </ul>
   );
